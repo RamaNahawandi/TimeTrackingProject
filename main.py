0affaaaeff0882c39e6c00329e5eeb05c6ac2827
SefaSAHAN <sefasahan35@gmail.com>
2022-10-30 12:43:09 +0100

    Time track app

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QDialog, QApplication
 from PyQt5.uic import loadUi
 import sys
+import json
+from email_validator import validate_email, EmailNotValidError
+
 
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QDialog, QApplication
@@ -12,10 +15,38 @@ class LoginUI(QDialog):
     def __init__(self):
         super(LoginUI,self).__init__()
         loadUi("./UI/login.ui",self)
+        self.list=['sefasahan35@gmail.com']
+        self.errorTextLogin.setText('')
+        self.errorTextSignUp.setText('')
 
         # This is example of changing screen
-        self.loginButton.clicked.connect(self.go_main_menu)
+        self.loginButton.clicked.connect(self.log_in)
+        self.signUpButton.clicked.connect(self.sign_up)
 
+
+    def log_in(self):
+        self.email=self.emailInputLogin.text()
+        if self.email in self.list:
+            self.go_main_menu()
+        else:
+            self.errorTextLogin.setText('Check your email adress or sign up please')
+            
+    def sign_up(self):
+        self.name=self.nameInputSignUp.text()
+        self.email=self.emailInputSignUp.text()
+        try:
+            v = validate_email(self.email)
+            self.email = v["email"] 
+            if self.email in self.list:
+                self.errorTextSignUp.setText('This mail is already exist')
+            else:
+                self.list.append(self.email)
+                self.go_main_menu()
+                
+        except EmailNotValidError as e:
+            self.errorTextSignUp.setText('Check email please, that is not a valid email')
+           
+            
     def go_main_menu(self):
         main_menu = MainMenuUI()
         widget.addWidget(main_menu)
@@ -52,6 +83,11 @@ UI = LoginUI() # This line determines which screen you will load at first
     # UI = PomodoroUI()
     # UI = ShortBreakUI()
     # UI = LongBreakUI()
+# this block is for make a pup.up message   
+# self.messagebox=QtWidgets.QMessageBox()
+# self.messagebox.setText(f"Check your email adress or sign up please")
+# self.messagebox.setWindowTitle('log in problem')
+# self.messagebox.exec_()
 
 widget = QtWidgets.QStackedWidget()
 widget.addWidget(UI)
